37ad1c20113294e927d408f481c4eed15f46545e
Feklistova Polina <pmfeklistova@miem.hse.ru>
2023-07-23 14:10:29 +0300

    +Models

---
+++ Practice/Models/TradedTicker.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text.Json.Serialization;
+using Practice.Utils.Converters;
+
+namespace Practice.Models
+{
+    public class TradedTicker
+    {
+        public string Ticker { get; set; }
+
+        [JsonConverter(typeof(DoubleJsonConverter))]
+        public double Price { get; set; }
+
+        [JsonPropertyName("change_amount")]
+        public string Change_Amount { get; set; }
+
+        [JsonPropertyName("change_percentage")]
+        public string Change_Percentage { get; set; }
+
+        [JsonConverter(typeof(IntJsonConverter))]
+        public int Volume { get; set; }
+
+        [JsonPropertyName("name")]
+        public string TickerDecryption { get; set; }
+    }
+}
---
+++ Practice/Models/TradedTickerStatistics.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text.Json.Serialization;
+
+namespace Practice.Models
+{
+    public class TradedTickerStatistics
+    {
+        public string Metadata { get; set; }
+
+        [JsonPropertyName("last_updated")]
+        public string LastUpdated { get; set; }
+
+        [JsonPropertyName("top_gainers")]
+        public List<TradedTicker> TopGainers { get; set; }
+
+        [JsonPropertyName("top_losers")]
+        public List<TradedTicker> TopLosers { get; set; }
+
+        [JsonPropertyName("most_actively_traded")]
+        public List<TradedTicker> MostActivelyTraded { get; set; }
+    }
+}
