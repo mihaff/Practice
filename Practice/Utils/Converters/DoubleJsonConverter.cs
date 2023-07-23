e94dd461e83a9dfd3ad6a113f62af75b04e746ee
Feklistova Polina <pmfeklistova@miem.hse.ru>
2023-07-23 12:36:16 +0300

    Utils. Converter

---
+++ Practice/Utils/Converters/DoubleJsonConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Practice.Utils.Converters
+{
+    public class DoubleJsonConverter : JsonConverter<double>
+    {
+        public override double Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            var str = reader.GetString() ?? "0";
+            str = str.Replace(".", ",");
+            var value = Convert.ToDouble(str);
+            return value;
+        }
+
+        public override void Write(Utf8JsonWriter writer, double value, JsonSerializerOptions options)
+        {
+            writer.WriteStringValue(value.ToString());
+        }
+    }
+}
---
+++ Practice/Utils/Converters/IntJsonConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Practice.Utils.Converters
+{
+    public class IntJsonConverter : JsonConverter<int>
+    {
+        public override int Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            var str = reader.GetString() ?? "0";
+            var value = int.Parse(str);
+            return value;
+        }
+
+        public override void Write(Utf8JsonWriter writer, int value, JsonSerializerOptions options)
+        {
+            writer.WriteStringValue(value.ToString());
+        }
+    }
+}
